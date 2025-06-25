4d2edb28291777bcd03bd7fa6ea71aea42f96c46
Burhan Fauzi Salam <burhanfauzisalam@gmail.com>
2025-06-25 09:03:47 +0700

    ...

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import mysql from "mysql2/promise";
 import cron from "node-cron";
 
 const PROMETHEUS_URL = `http://localhost:9090/api/v1/query?query=up{job="blackbox-icmp"}`;
+const PROBE_DURATION_SECONDS_URL = `http://localhost:9090/api/v1/query?query=probe_duration_seconds{job="blackbox-icmp"}`;
 const db = mysql.createPool({
   port: 3308,
   host: "localhost",
@@ -13,6 +14,7 @@ const db = mysql.createPool({
 });
 
 cron.schedule("* * * * *", async () => {
+  probe();
   try {
     const response = await axios.get(PROMETHEUS_URL);
     const results = response.data.data.result;
@@ -35,3 +37,25 @@ cron.schedule("* * * * *", async () => {
     );
   }
 });
+
+const probe = async () => {
+  try {
+    const response = await axios.get(PROBE_DURATION_SECONDS_URL);
+    const results = response.data.data.result;
+
+    for (const item of results) {
+      const instance = item.metric.instance;
+      const status = parseInt(item.value[1]);
+
+      await db.execute(
+        "INSERT INTO probe_duration_seconds (instance, status) VALUES (?, ?)",
+        [instance, status]
+      );
+    }
+  } catch (error) {
+    console.error(
+      "Error querying Prometheus or writing to MySQL:",
+      err.message
+    );
+  }
+};
